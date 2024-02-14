758679d8afdc965aa9adf8883e7fc70203614856
Abdallah Montassar_bellah <amontasar2@gmail.com>
2024-02-14 10:30:05 +0100

    template modifier + radiobutton + function done

--- app/src/main/java/com/example/imc_app_mba/MainActivity.java
+++ app/src/main/java/com/example/imc_app_mba/MainActivity.java
@@ -13,51 +13,72 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     EditText txtpoids;
     EditText txtTaille;
-    EditText txtAge;
     Button btnCalc;
     TextView result;
     ImageFilterView lblresulta;
-    RadioButton rdHomme;
+    RadioButton rdFemme;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtpoids = (EditText) findViewById(R.id.txtpoids);
-        txtTaille = (EditText) findViewById(R.id.txtTaille);
-        txtAge = (EditText) findViewById(R.id.txtAge);
-        btnCalc = (Button) findViewById(R.id.btnCalc);
-        result = (TextView) findViewById(R.id.result);
-        lblresulta = (ImageFilterView) findViewById(R.id.lblresulta);
-        rdHomme = (RadioButton) findViewById(R.id.rdHomme);
+
+        txtpoids = findViewById(R.id.txtpoids);
+        txtTaille = findViewById(R.id.txtTaille);
+        btnCalc = findViewById(R.id.btnCalc);
+        result = findViewById(R.id.result);
+        lblresulta = findViewById(R.id.lblresulta);
+        rdFemme = findViewById(R.id.rdFemme);
+
 
         btnCalc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (txtpoids.getText().length()>0 && txtTaille.getText().length()>0){
+                if (txtpoids.getText().length() > 0 && txtTaille.getText().length() > 0) {
                     float Taille = Float.parseFloat(txtTaille.getText().toString());
                     float Poids = Float.parseFloat(txtpoids.getText().toString());
                     Float imc = CalculIMC(Taille, Poids);
 
-                    if (imc <16){
-                        result.setText("votre IMC est:"+imc+"Vous étes trop maigre");
-                        lblresulta.setImageResource(R.drawable.maigreur);
-                    } else if (imc<18.5){
-                        result.setText("votre IMC est:"+imc+"vous étes maigre ");
-                        lblresulta.setImageResource(R.drawable.maigreur);
-                    } else if (imc <25){
-                        result.setText("votre IMC est:"+imc+"vous étes normale ");
-                        lblresulta.setImageResource(R.drawable.normale);
-                    } else if (imc <30){
-                        result.setText("votre IMC est:"+imc+"vous étes Gros(se)");
-                        lblresulta.setImageResource(R.drawable.surpoids);
-                    } else {
-                        result.setText("votre IMC est:"+imc+"vous étes Obèse");
-                        lblresulta.setImageResource(R.drawable.obese);
+                    // Obtenez la valeur de la balise (tag) du bouton radio sélectionné
+                    int selectedSexe = rdFemme.isChecked() ? 0 : 1; // 0 pour Femme, 1 pour Homme
 
+
+                    if (selectedSexe == 0) { // Femme
+                        if (imc < 16) {
+                            result.setText("Votre IMC est:" + imc + " \n Vous êtes trop maigre");
+                            lblresulta.setImageResource(R.drawable.maigreurf);
+                        } else if (imc < 18.5) {
+                            result.setText("Votre IMC est:" + imc + " \n Vous êtes maigre");
+                            lblresulta.setImageResource(R.drawable.maigreurf);
+                        } else if (imc < 25) {
+                            result.setText("Votre IMC est:" + imc + " \n Vous êtes normale");
+                            lblresulta.setImageResource(R.drawable.normalef);
+                        } else if (imc < 30) {
+                            result.setText("Votre IMC est:" + imc + " \n Vous êtes Grosse");
+                            lblresulta.setImageResource(R.drawable.surpoidsf);
+                        } else {
+                            result.setText("Votre IMC est:" + imc + " \n Vous êtes Obèse");
+                            lblresulta.setImageResource(R.drawable.obesef);
+                        }
+                    } else { // Homme
+                        if (imc < 16) {
+                            result.setText("Votre IMC est:" + imc + " \n Vous êtes trop maigre");
+                            lblresulta.setImageResource(R.drawable.maigreur);
+                        } else if (imc < 18.5) {
+                            result.setText("Votre IMC est:" + imc + " \n Vous êtes maigre");
+                            lblresulta.setImageResource(R.drawable.maigreur);
+                        } else if (imc < 25) {
+                            result.setText("Votre IMC est:" + imc + " \n Vous êtes normale");
+                            lblresulta.setImageResource(R.drawable.normale);
+                        } else if (imc < 30) {
+                            result.setText("Votre IMC est:" + imc + " \n Vous êtes Gros");
+                            lblresulta.setImageResource(R.drawable.surpoids);
+                        } else {
+                            result.setText("Votre IMC est:" + imc + " \n Vous êtes Obèse");
+                            lblresulta.setImageResource(R.drawable.obese);
+                        }
                     }
-
                 }
             }
         });
